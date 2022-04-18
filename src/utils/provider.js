47f1545a035b6d13149f29cb8e13f5086abacbb4
chenjie <745960380@qq.com>
2022-04-18 17:18:49 +0800

    更新网络

--- src/utils/provider.js
+++ src/utils/provider.js
@@ -2,7 +2,7 @@ import Web3 from 'web3';
 
 const RPC_URL = () => {
   // return 'http://localhost:8545';
-  return 'https://rinkeby.infura.io/v3/13ab66893f804b6684194366db26efc3';
+  return 'https://eth-rinkeby.alchemyapi.io/v2/1sghkDK1jKFZB7LeCdBr52HI1BHO_A6l';
 };
 
 export const simpleRpcProvider = new Web3(new Web3.providers.HttpProvider(RPC_URL()));
